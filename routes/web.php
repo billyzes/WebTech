81ee99455a255436e2e773c3ff7709351cf734cf
BILLY PENDON MAGALONA <2201165@cc.usls.edu>
2025-01-30 11:15:41 +0800

    Finished routing

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login' ,'App\Http\Controllers\LoginController@index' );
+
+Route::get('inventory','App\Http\Controllers\InventoryController@index');
+
