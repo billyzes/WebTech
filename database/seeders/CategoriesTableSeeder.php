0b508cf831b2f20948ea3ecca5969e3be6aff2a3
BILLY PENDON MAGALONA <2201165@cc.usls.edu>
2025-03-13 11:22:34 +0800

    Displaying categories data with using laravel directives

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -14,6 +14,9 @@ class CategoriesTableSeeder extends Seeder
             ['id' => 1, 'name' => 'Technology', 'description' => 'Latest gadgets and software.'],
             ['id' => 2, 'name' => 'Health', 'description' => 'Tips and products for a healthy life.'],
             ['id' => 3, 'name' => 'Education', 'description' => 'Resources for students and teachers.'],
+            // ['id' => 4, 'name' => 'Technologysss', 'description' => 'adsdasdas.'],
+            // ['id' => 5, 'name' => 'Health', 'description' => 'dasdasdas.'],
+            // ['id' => 6, 'name' => 'Education', 'description' => 'rwqrqwrq'],
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'Welcome';
-});
 
-Route::get('/hello' ,'App\Http\Controllers\HelloController@index' );
+Route::get('/' ,'App\Http\Controllers\HelloController@index' );
 Route::get('/create' ,'App\Http\Controllers\CreateController@index' );
+
+Route::get('/categories', 'App\Http\Controllers\CategoryController@index');
 
 Route::get('/login' ,'App\Http\Controllers\LoginController@index' );
 Route::post('/login' ,'App\Http\Controllers\LoginController@auth' );
